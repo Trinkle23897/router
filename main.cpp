daaf713d75b8b515ddc0ac8273a93d67f88ba22c
Trinkle <463003665@qq.com>
2018-11-10 11:44:26 +0800

    finish

--- main.cpp
+++ main.cpp
@@ -51,7 +51,8 @@ void analyseIP(ip*iphd)
 
 int main(int argc, char** argv)
 {
-	char skbuf[1512], *data;
+	const uint32_t SOCKET_LEN = 1512;
+	char skbuf[SOCKET_LEN], *data;
 	int32_t recvfd, datalen, recvlen;
 	pthread_t tid;
 	ip *ip_recvpkt;
@@ -130,9 +131,8 @@ int main(int argc, char** argv)
                     fprintf(stderr, "could not find %s mac addr\n", nexthopinfo->ifname);
                 }
                 close(sockfd);
-                //ip_transmit(iphead, );
-				// 调用计算校验和函数count_check_sum，返回新的校验和
 				iphead->ip_sum = count_check_sum(iphead, true);
+				// 调用计算校验和函数count_check_sum，返回新的校验和
 				// send ether icmp
 				// 调用ip_transmit函数   填充数据包，通过原始套接字从查表得到的出接口(比如网卡2)将数据包发送出去
 				// 将获取到的下一跳接口信息存储到存储接口信息的结构体ifreq里，通过ioctl获取出接口的mac地址作为数据包的源mac地址
@@ -140,31 +140,22 @@ int main(int argc, char** argv)
 				// <1>.根据获取到的信息填充以太网数据包头，以太网包头主要需要源mac地址、目的mac地址、以太网类型eth_header->ether_type = htons(ETHERTYPE_IP);
 				// <2>.再填充ip数据包头，对其进行校验处理；
 				// <3>.然后再填充接收到的ip数据包剩余数据部分，然后通过raw socket发送出去
-				
+				int sendfd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW);
+				sockaddr_ll sadr_ll;
+				sadr_ll.sll_ifindex = nexthopinfo->ifindex;
+				sadr_ll.sll_halen = ETH_ALEN;
+				// mac_addr_to is the result of arp query
+				memcpy(sadr_ll.sll_addr, srcmac->mac, ETH_ALEN);
+				// length should be equal to the length you receive from raw socket
+				if (sendto(sendfd, skbuf, SOCKET_LEN, 0,
+					(const sockaddr*)&sadr_ll, sizeof(sockaddr_ll)) == -1) {
+					fprintf(stderr, "send error\n");
+				} else
+					fprintf(stderr, "send succeed\n");
+				close(sendfd);
 			}
 		}
 	}
 	close(recvfd);
 	return 0;
 }
-/*
-查某个 interface 的 MAC 的方法:
-*/
-/*
-发包的方法:
-首先确保 IP 头、以太网头都正确修改了。
-int sendfd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW);
-struct sockaddr_ll sadr_ll;
-sadr_ll.sll_ifindex = 0; // index of next hop
-sadr_ll.sll_halen = ETH_ALEN;
-// mac_addr_to is the result of arp query
-memcpy(sadr_ll.sll_addr, mac_addr_to, ETH_ALEN);
-// length should be equal to the length you receive from raw socket
-if ((result = sendto(sendfd, skbuf, length, 0,
-(const struct sockaddr *)&sadr_ll,sizeof(struct sockaddr_ll))) == -1) {
-// send error
-} else {
-// send succeed
-}
-close(sendfd);
-*/
